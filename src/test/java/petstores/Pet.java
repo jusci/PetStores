2e3fec0c50b9a3036922d7fda138b201b8dc2994
Juscilene Rufino <juscilne.reis@gmail.com>
2021-08-26 22:27:53 -0300

    Criação do teste para incluir um pet

---
+++ src/test/java/petstores/Pet.java
@@ -0,0 +1,57 @@
+//1 - Pacote
+
+package petstores;
+
+// 2 - Bibliotecas
+
+// 3- Classe
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
+public class Pet {
+
+    //3.1 - Atributos
+
+    String uri="https://petstore.swagger.io/v2/pet";//endereço da entidade Pet
+
+
+    //3.2 - metodos e funçoes
+
+
+    public String lerJson (String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
+
+    //Incluir- Creat - Post
+    @Test // identifica o metodo ou funcao como um teste para o TestNG ( mesma função que o Junit)
+    public void incluirPet() throws IOException {
+
+        String jsonBody = lerJson("db/pet1.json");
+
+        //Sintaxe Gherkin
+        //Dado - Quando - Então
+        //Give - When - Then
+
+        given()
+                .contentType("application/json") //comum em API REST - antigas era "Text/XML"
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .post(uri)
+
+        .then()
+                .log().all()
+                .statusCode(200)
+
+        ;
+
+
+    }
+
+}
