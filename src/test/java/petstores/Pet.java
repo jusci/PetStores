18ad8e0a46b49466accf74b2d2527a38efd056aa
Juscilene Rufino <juscilne.reis@gmail.com>
2021-08-27 23:08:12 -0300

    Completei o script incluirPet (Get)

--- src/test/java/petstores/Pet.java
+++ src/test/java/petstores/Pet.java
@@ -21,10 +21,10 @@ public class Pet {
 
     //3.1 - Atributos
 
-    String uri="https://petstore.swagger.io/v2/pet";//endereÃ§o da entidade Pet
+    String uri="https://petstore.swagger.io/v2/pet";//endereço da entidade Pet
 
 
-    //3.2 - metodos e funÃ§oes
+    //3.2 - metodos e funçoes
 
 
     public String lerJson (String caminhoJson) throws IOException {
@@ -32,13 +32,15 @@ public class Pet {
     }
 
     //Incluir- Creat - Post
-    @Test // identifica o metodo ou funcao como um teste para o TestNG ( mesma funÃ§Ã£o que o Junit)
+    @Test(priority =1) // estabelece prioridade na ordem da execução, quando se executa pelo build.grandle
+    //Se tentar executar diretamente pela classe essa ordem nao é respeitada
+    //@Test identifica o metodo ou funcao como um teste para o TestNG ( mesma função que o Junit)
     public void incluirPet() throws IOException {
 
         String jsonBody = lerJson("db/pet1.json");
 
         //Sintaxe Gherkin
-        //Dado - Quando - EntÃ£o
+        //Dado - Quando - Então
         //Give - When - Then
 
         given()
@@ -53,11 +55,41 @@ public class Pet {
                 .statusCode(200)
                 .body("name", is("Jimmy"))
                 .body("status",is("available"))
-                .body("category.name",is("dog"))//usa is quando nao tem []
-                .body("tags.name",contains("sta"))//usa quando tem []
+                .body("category.name",is("AXL345789POWS"))//usa is quando nao tem []
+                .body("tags.name",contains("data"))//usa quando tem []
         ;
+
+    }
+
+    @Test (priority =2) // estabelece prioridade na ordem da execução, quando se executa pelo build.grandle
+
+    public void consultarPet(){
+
+        String petId="1985031610";
+        String token =
+
+        given()
+                .contentType("application/json")
+                .log().all()
+
+        .when()
+                .get(uri + "/" + petId)
+
+        .then()
+
+                .statusCode(200)
+                .body("name", is("Jimmy"))
+                .body("status",is("available"))
+                .body("category.name",is("AXL345789POWS"))//usa is quando nao tem []
+                //.body("tags.name",contains("data"))//usa quando tem []
+        .extract()
+                .path("category.name")
+        ;
+
+        System.out.println("O token é: " + token);
 
 
     }
+
 
 }
